fb1513630f3b76dc2ec807aefedaf2ee2e2ee7b7
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-07-13 02:05:04 +0000

    seems trx is required
    svn path=/trunk/acsis/specwrite/; revision=10029

--- specwrite.h
+++ specwrite.h
@@ -115,6 +115,7 @@ typedef struct ACSISRtsState {
   int    acs_spec_window_id; /* SPW id used to assign subsystem */
   unsigned int acs_feed;  /* Feed number */
   float  acs_tsys;
+  float  acs_trx;
   int    acs_no_prev_ref;
   int    acs_no_next_ref;
   int    acs_no_ons;
